7e432aad0da63f14a303fa493adcbd2e38d7bfd7
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-31 20:19:32 +0530

    react router

--- React-Router/src/App.jsx
+++ React-Router/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+
 
 function App() {
   return (
     <>
-     <h1>code is running ig</h1>
+     <h1 className='bg-green-300' >code is running ig</h1>
+    
+    <Home/>
+    <Header/>
+    <Footer/>
     </>
+
   )
 }
 
