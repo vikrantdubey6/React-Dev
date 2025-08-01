dff84ee9855efa46e2516b00f47713fc343ab935
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-08-01 21:14:00 +0530

    Context api

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
  
 
   return (
-    <>
-      <h1>Jai Bhole</h1>
-    </>
+    
+     <UserContextProvider>
+
+        <h1>Jai Mata Di</h1>
+      
+      <Login/>
+      <Profile/>
+
+
+     </UserContextProvider>
+    
   )
 }
 
