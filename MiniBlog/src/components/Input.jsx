886c62c802a76cf26e8a1e882c50832706212785
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-08-07 16:08:17 +0530

    Mini Blog

---
+++ MiniBlog/src/components/Input.jsx
@@ -0,0 +1,32 @@
+import React, { useId } from 'react';
+
+function Input(
+  { 
+    label, 
+    type = 'text', 
+    className = '', 
+    ref,        // pull in the ref  
+    ...props    // everything else  
+  }
+) {
+  const id = useId();
+
+  return(
+    <div className='w-full'>
+        {label && <label 
+        className='block mb-1' 
+        htmlFor={id}>
+            {label}
+            </label>}
+
+            <input
+            type={type}
+            ref = {ref}
+            {...props}
+            id={id}
+            className={` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`} 
+            />
+    </div>
+  )
+}
+export default Input
--- MiniBlog/src/components/index.js
+++ MiniBlog/src/components/index.js
@@ -1,9 +1,16 @@
-import Footer from "./footer/Footer";
-import Header from "./header/Header";
+import Footer from "./Footer/footer";
+import Header from "./Header/header";
+import Container from "./container/Container";
+import Logo from "./logo";
+import LogoutBtn from './Header/LogoutBtn'
+
 
 
 export{
     Header,
     Footer,
+    Container,
+    Logo,
+    LogoutBtn
     
 }
